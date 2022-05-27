2e48d621468a4370b39c59639356dce50d4747d2
shim <david532498@gmail.com>
2022-05-28 02:54:31 +0900

    case: add

--- [PROJECT_ROOT]/src/Player.c
+++ [PROJECT_ROOT]/src/Player.c
@@ -1,1 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+//#include "node.h"
+//#include "linkedlist.h"
+//#include "textfilewriter.h"
 
+int main(){
+	
+	int a,b;
+	scanf("%d", &a);
+	char new_data[100];
+	for (int i=0;i<a;i++){
+		scanf("%s", new_data);
+		append(100, new_data);
+	}
+
+	scanf("%d", &b);
+	for (int j=0;j<b;j++){
+		char ch;
+		char title[100];
+		scanf("%c", &ch);
+		switch (ch){
+			case 'add':
+				append_left(100, title);
+				break;
+		}
+	}
+}
